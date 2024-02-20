a160103fe7c23e453815b070d88642562546d24d
VietNamCombatZ <127002473+YoshiGamingNoYT@users.noreply.github.com>
2024-02-20 21:13:23 +0700

    js

---
+++ buoi_6/js1.js
@@ -0,0 +1,16 @@
+const check = "false";
+
+if (check) {
+  console.log("Dung");
+} else {
+  console.log("Sai");
+}
+
+//null, false, NaN, undefined, 0 thì là sai
+const check1 = null;
+
+if (check1) {
+  console.log("Dung");
+} else {
+  console.log("Sai");
+}
