59ae545fcb6f259940face77ad1e9c2f9287021c
VietNamCombatZ <127002473+YoshiGamingNoYT@users.noreply.github.com>
2024-02-22 20:58:01 +0700

    buoi_7

---
+++ buoi_7/bai_giang/practice_1.js
@@ -0,0 +1,25 @@
+const date1 = new Date();
+const date2 = new Date();
+
+date1.getHours();
+console.log(
+  date1.getHours() +
+    "-" +
+    date1.getMinutes() +
+    "-" +
+    date1.getSeconds() +
+    " " +
+    date1.getDay() +
+    "-" +
+    date1.getMonth() +
+    "-" +
+    date1.getFullYear()
+);
+console.log(
+  date2.getDay() + "/" + date2.getMonth() + "/" + date2.getFullYear()
+);
+
+//for(index in object or array)
+
+// for (index of array )
+
