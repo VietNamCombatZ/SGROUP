cf5acf51067d95020058998c3ddf39600ccd5a4b
VietNamCombatZ <127002473+YoshiGamingNoYT@users.noreply.github.com>
2024-02-29 16:48:24 +0700

    15

---
+++ landing_page/pop_up_video_onclick.js
@@ -0,0 +1,7 @@
+document.getElementById("openPopup").addEventListener("click", function () {
+  document.getElementById("videoPopup").style.display = "block";
+});
+
+document.getElementById("closePopup").addEventListener("click", function () {
+  document.getElementById("videoPopup").style.display = "none";
+});
