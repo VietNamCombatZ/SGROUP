7bb0cf0e536a77b09cc70edbfeb6d1e8549180bf
VietNamCombatZ <127002473+YoshiGamingNoYT@users.noreply.github.com>
2024-03-28 19:28:57 +0700

    01

---
+++ Trello_final_project/index.js
@@ -0,0 +1,28 @@
+
+// function to turn on display add_task_section
+function Display ()
+{
+    let addTaskSect = document.getElementById("add_task_section");
+    let addTaskBtn = document.getElementById("add_task-btn");
+
+    addTaskSect.style.display = "flex";
+
+    // addTaskBtn.addEventListener("click", () => {
+    //     addTaskSect.style.display = "flex";
+    // })
+}
+
+// function to turn off display add_task_section
+function Disappear ()  
+{
+    let addTaskSect = document.getElementById("add_task_section");
+    addTaskSect.style.display = "none";
+
+}
+
+
+//function to create work_todo_card
+function createElement()
+{
+    
+}
