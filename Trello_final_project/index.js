b353608641497a386b4ef47b888946a12e805b27
VietNamCombatZ <127002473+YoshiGamingNoYT@users.noreply.github.com>
2024-04-02 19:04:45 +0700

    01

--- Trello_final_project/index.js
+++ Trello_final_project/index.js
@@ -1,28 +1,61 @@
-
 // function to turn on display add_task_section
-function Display ()
-{
-    let addTaskSect = document.getElementById("add_task_section");
-    let addTaskBtn = document.getElementById("add_task-btn");
+function Display() {
+  let addTaskSect = document.getElementById("add_task_section");
+  let addTaskBtn = document.getElementById("add_task_btn");
 
-    addTaskSect.style.display = "flex";
+  addTaskSect.style.display = "flex";
 
-    // addTaskBtn.addEventListener("click", () => {
-    //     addTaskSect.style.display = "flex";
-    // })
+  // addTaskBtn.addEventListener("click", () => {
+  //     addTaskSect.style.display = "flex";
+  // })
 }
 
 // function to turn off display add_task_section
-function Disappear ()  
-{
-    let addTaskSect = document.getElementById("add_task_section");
-    addTaskSect.style.display = "none";
-
+function Disappear() {
+  let addTaskSect = document.getElementById("add_task_section");
+  addTaskSect.style.display = "none";
 }
 
-
 //function to create work_todo_card
-function createElement()
-{
-    
+function createElement() {}
+
+//function to check whether input field are fullfilled
+
+// ! bug 
+function checkCategoryValidate() {
+  let categoryInput = document.getElementById("add_task_category").value;
+  let categoryTitle = document.getElementById("add_task_title").value;
+  let categoryContent = document.getElementById("add_task_content").value;
+
+  let categoryCheckAll = document.querySelectorAll(".add_task_input");
+
+  
+//   categoryCheckAll.forEach((categoryCheck) => {
+//     categoryCheck.classList.remove("active");
+//   });
+
+  console.log(categoryTitle);
+  if (categoryInput == "") {
+    categoryInput.classList.add("active");
+  }
+  else
+  {
+    categoryInput.classList.remove("active");
+  }
+
+  if (categoryTitle == "") {
+    categoryTitle.classList.add("active");
+  }
+  else
+  {
+    categoryTitle.classList.remove("active");
+  }
+
+  if (categoryContent == "") {
+    categoryContent.classList.add("active");
+  }
+  else
+  {
+    categoryContent.classList.remove("active");
+  }
 }
